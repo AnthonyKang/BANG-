82016ecde7fcaec2ae753337cb6a973d7beedc07
Wells Lin <wellslin94@gmail.com>
2015-12-27 18:08:11 -0800

    Updating pom file

--- bang/src/test/java/game/MainTest.java
+++ bang/src/test/java/game/MainTest.java
@@ -1,7 +1,13 @@
 package game;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class MainTest extends TestCase {
-
+	
+	@Test
+	public void test() {
+		assertEquals(true, true);
+	}
 }
