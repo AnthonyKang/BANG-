8c3ec21a084d16c5e0664ddd333a9ca77ea660e5
grantlam <gralam@ucdavis.edu>
2016-01-05 14:51:45 -0800

    Added player package.

---
+++ bang/src/main/java/player/Character.java
@@ -0,0 +1,10 @@
+package player;
+
+import lombok.Data;
+
+@Data
+public class Character {
+    private final String name;
+    private final String ability;
+    private final int maxLife;
+}
---
+++ bang/src/main/java/player/Role.java
@@ -0,0 +1,5 @@
+package player;
+
+public enum Role {
+    SHERIFF, DEPUTY, OUTLAW, RENEGADE
+}
